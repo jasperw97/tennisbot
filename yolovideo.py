0f78df0e3b50f0f6f0d1a4ad2a73656762c6e6d3
jasperw97 <jasperdeanwei@gmail.com>
2025-12-07 14:57:01 -0500

    added visualization of prediction box

--- yolovideo.py
+++ yolovideo.py
@@ -5,7 +5,7 @@ import numpy as np
 
 model = YOLO("runs/detect/train3/weights/best.pt")
 
-cap = cv2.VideoCapture("tennis2.MPG")
+cap = cv2.VideoCapture("tsitsipas.mp4")
 
 if not cap.isOpened():
     print("Error, can't open video file")
@@ -15,6 +15,7 @@ else:
     
     
 #State Variables
+extended_roi = []
 yolo_detected = False
 framenum = 0
 frames = []
@@ -22,17 +23,22 @@ detects = []
 velocity = [] #initial state velocity
 
 while True:
-    if len(velocity) != 0:
-        update_roi(extended_roi, velocity)
-    
     framenum += 1
     ret, frame = cap.read()
     frames.append(frame)
     copy = frame.copy()
+    if len(velocity) != 0:
+        update_roi(extended_roi, velocity)
+    
+    #Trying to visualize where the roi is to see how well we are trackin the ball
+    if len(extended_roi) != 0:
+        cv2.rectangle(copy, (int(extended_roi[0]), int(extended_roi[1])), (int(extended_roi[2]), int(extended_roi[3])), (255, 0, 0), 3)
+        
     if len(frames) > 2:
         frames = frames[1:]
     if len(frames) == 2:
         mask = get_mask(frames[0], frames[1])
+        cv2.imshow("mask", mask)
     # if frame is read correctly ret is True
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
@@ -43,7 +49,7 @@ while True:
             for box in r.boxes:
                 x1, y1, x2, y2 = box.xyxy[0]
                 #Checking if this detection is within motion mask
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) 
+                x1, y1, x2, y2 = int(x1) - 3, int(y1) - 3, int(x2) + 3, int(y2) + 3
                 mask_roi = mask[y1:y2, x1:x2]
                 if np.any(mask_roi != 0):
                     yolo_detected = True
@@ -65,10 +71,13 @@ while True:
                         detects = detects[1:]
                     velocity = [(detects[1][0] - detects[0][0]) / (detects[1][4] - detects[0][4]), (detects[1][1] - detects[0][1]) / (detects[1][4] - detects[0][4])] # [vx, vy]
                     cv2.rectangle(copy, (int(current_detect[0]), int(current_detect[1])), (int(current_detect[2]), int(current_detect[3])), (0, 255, 0), 2)
+                
+                #First reset methods
                 elif len(contours) == 0:
                     # Resets when there is no detections
                     detects = []
                     velocity = []
+                    extended_roi = []
                     
                 
 
